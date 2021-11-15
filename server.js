277d3b817755cbabf093675d13f6c7553cfbbcc1
jheckel <jalen.heckel@icloud.com>
2021-11-15 10:46:11 -0800

    updated file structure to pull server file out from developer folder and removed developer folder.

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+const path = require("path");
+
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/notes", (request, response) => {
+    response.sendFile(path.join(__dirname, "..", "..", "notes.html"));
+    console.log("Your Notes!");
+
+})
+
+app.get("/", (request, response) => {
+    // response.sendFile(path.join(__dirname, "..", "..", "index.html"));
+    response.sendFile(path.join(__dirname, "index.html"));
+    console.log("Your index!");
+})
+
+app.get("/api/notes", (request, response) => {
+
+    fs.readFile(path.join(__dirname, "..", "..", "..", "db.json"), function (err, data) {
+        console.log("API Notes!");
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+});
